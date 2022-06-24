cc31fb7d403636f62d1aef7f3799237a70b1be7a
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-06-24 14:06:53 +0100

    Hid the secret key

--- I4G002308ZYP/settings.py
+++ I4G002308ZYP/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(scfmuab04xl6(#9hky9n(2nx3r^m)qc^wcesc1t$-87j3b#@o'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
